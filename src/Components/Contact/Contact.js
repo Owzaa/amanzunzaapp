7e5e2519e5ac89085062c3dd7704396ec1fc7e48
Owzaa <nyondo.olwethu@gmail.comm>
2022-04-05 00:46:35 +0200

    commit on master

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {Component} from '' 
+
+class ContactForm extends React.Component () {
+    render(){
+
+
+        return (
+
+
+            <div className="form__Contact" id="form__Container">
+
+            <form className="form__Contact">
+                
+            </form>            
+
+        <label className="text__Label"></label>
+
+
+
+            </div>
+        )
+
+
+    }
+
+
+}
